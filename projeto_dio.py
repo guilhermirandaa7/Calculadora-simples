34419b67044ef29a6e4b9822bf8b60f46a7f5b2f
guilhermirandaa7 <guilherme_2g@hotmail.com>
2026-04-16 15:18:58 -0300

    adicionando loop na calculadora

--- projeto_dio.py
+++ projeto_dio.py
@@ -1,14 +1,29 @@
 def soma(a, b):
     return a + b 
+
 def subtracao(a, b): 
     return a - b
+
 def multiplicacao(a,b):
     return a * b
+
 def divisão(a, b):
     if b==0:
         return 'erro: divisão por zero' 
     return a / b 
 
+while true: 
+    print ("\n===CALCULADORA===")
+    print ("1 - Soma")
+    print ("2 - Subtração")
+    print ("3 - Multiplicação")
+    print ("4 - Divisão")
+
+    
+    
+           
+           
+           )
 op = (input("Escolha uma operação"))
 
 if op == "1":
