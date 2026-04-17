4b1af91b51c860f1fea434201108741580667394
guilhermirandaa7 <guilherme_2g@hotmail.com>
2026-04-17 01:57:50 -0300

    Final da calculadora simples

--- projeto_dio.py
+++ projeto_dio.py
@@ -4,16 +4,16 @@ def soma(a, b):
 def subtracao(a, b): 
     return a - b
 
-def multiplicacao(a,b):
+def multiplicacao(a, b):
     return a * b
 
-def divisão(a, b):
-    if b==0:
-        return 'erro: divisão por zero' 
+def divisao(a, b):
+    if b == 0:
+        return "Erro: divisão por zero" 
     return a / b 
 
-while true: 
-    print ("\n===CALCULADORA===")
+while True: 
+    print ("\n=== CALCULADORA ===")
     print ("1 - Soma")
     print ("2 - Subtração")
     print ("3 - Multiplicação")
@@ -22,15 +22,29 @@ while true:
     op = input("Escolha:")
 
     if op not in ["1", "2", "3", "4"]:
-           print("Opção inválida:") 
-           continue  
+        print("Opção inválida:") 
+        continue
 
-try: 
-    num1 = float(input("Número 1: "))
-    num2 = float(input("Número 2: "))
-except ValueError:
-    print("Entrada inválida!")
-    
+    try: 
+        num1 = float(input("Número 1: "))
+        num2 = float(input("Número 2: "))
+    except:
+        print("Entrada inválida!")
+        continue
+
+    if op == "1": 
+        print("Resultado", soma(num1, num2))
+    elif op =="2":
+        print("Resulatado", subtracao(num1, num2))
+    elif op == "3":
+        print("Resultado", multiplicacao(num1, num2))
+    elif op=="4":
+        print("Resultado", divisao(num1, num2))
+
+    sair = input("Deseja sair? (s/n): ")
+    if sair.lower() == "s":
+        break
+
 
   
     
